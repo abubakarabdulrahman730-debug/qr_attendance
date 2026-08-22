f31c0b64267dfda4b71b7d4ceb732c366ca515a6
abdullahi <abdallahsalihu1@gmail.com>
2026-08-22 13:52:19 +0100

    update

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+<<<<<<< HEAD
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -14,5 +15,21 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getDatabase(app);
+=======
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD60h8U6D8gdzW4_-oCHnjHbc85aNflNmg",
+  authDomain: "attendly-5bb6f.firebaseapp.com",
+  projectId: "attendly-5bb6f",
+  storageBucket: "attendly-5bb6f.firebasestorage.app",
+  messagingSenderId: "536516052846",
+  appId: "1:536516052846:web:66368776eda9c8dd2ea37f"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+>>>>>>> 895c4319261db92b8ee105a43855686f89294661
 
 export { db };
